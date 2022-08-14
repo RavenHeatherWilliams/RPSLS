dfbf3fafdeff51345a04a49376358fb55e564910
Raven Williams <heatherwilliams.raven@gmail.com>
2022-08-13 22:45:30 -0500

    Noted requirements for assignment

---
+++ main.py
@@ -0,0 +1,10 @@
+#Create GitHub
+#Create main.py(created first)
+# 10 Commits
+# Incorporate inheritance into game
+# Account for and handle bad user input, ensuring that any user input is validated and reoptained if necessary
+# Store all of the guesture options/choiced in a list. 
+# Utilize the list of gestures within the code
+# Correct player need to win the round based on the choices made by each player 
+# Best of three rounds (will require a while   loop)
+# Create the option of a single player (human vs AI) or multiplayer (human vs human) game 
