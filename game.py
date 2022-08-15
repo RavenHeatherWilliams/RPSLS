86cfeb069fead65b2c3e4128501269508867a3aa
Raven Williams <heatherwilliams.raven@gmail.com>
2022-08-14 19:23:15 -0500

    Created the three different functions Created the player class and function.

--- game.py
+++ game.py
@@ -13,6 +13,22 @@ class Game:
 
     def display_welcome(self):
         print("Welcome to ROCK PAPER SCISSORS LIZARD SPOCK!")
+        print("Each match will be best of three games!")
+        print("Use the number keys to enter your selection")
+        
+               
+    def self.battle(self):
+        rock crushes scissors
+        scissors cuts paper 
+        paper covers rock 
+        rock crushes lizard 
+        lizard poisons spock 
+        spock smashes scissors
+        scissors decapitates lizard
+        lizard eats paper 
+        paper disproves spock 
+        spock vaporizes rock 
+
 
         # self.rock
         # self.scissors
